f35f7dfffe24439459b94be68420fae4b9cea3b2
maqboolsubair181 <maqboolsubair181@gmai.com>
2025-10-06 12:14:31 +0530

    V1

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "<h1>Hello from a traditional deployment! Jenkins CI/CD is working! 🚀</h1>"
+
+if __name__ == '__main__':
+    # This part is for local testing; Gunicorn will run the app in production
+    app.run(host='0.0.0.0', port=5000)
